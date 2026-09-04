d7618b02d794483ad3fdea916a9e5d762cdb779b
joaopedrocostalog-netizen <joaopedro.costalog@gmail.com>
2026-09-04 16:38:06 -0300

    Aplica novo layout do cartão principal

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './matrix-overlay.css';
 import './clients-tab.css';
+import './home-cleanup.css';
 import './clients-tab';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
