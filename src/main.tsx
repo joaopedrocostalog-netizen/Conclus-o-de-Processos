6a55df890920c89555ce996dbfca0b9a00f17ce0
joaopedrocostalog-netizen <joaopedro.costalog@gmail.com>
2026-09-04 11:55:16 -0300

    Ativa aba Clientes no cartão principal

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './matrix-overlay.css';
+import './clients-tab.css';
+import './clients-tab';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>
