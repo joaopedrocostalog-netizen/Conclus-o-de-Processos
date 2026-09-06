4c6366b8bdce196f16a3fcb8b28c59f8577ac8c8
joaopedrocostalog-netizen <joaopedro.costalog@gmail.com>
2026-09-06 04:53:21 -0300

    feat: enable individual copy buttons in reports

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,11 @@ import './home-cleanup.css';
 import './report-sources.css';
 import './client-detail-no-scroll.css';
 import './client-analysis-progress.css';
+import './client-field-copy.css';
 import './clients-tab';
 import './report-preview-loader';
 import './client-analysis-progress';
+import './client-field-copy';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>
